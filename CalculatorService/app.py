eb6fa9cf62f7151acd947f195def29b72ab955fa
Lucas BRANCOLINI <lucas.brancolini@efrei.net>
2025-02-23 17:32:36 +0100

    le logger stp

--- CalculatorService/app.py
+++ CalculatorService/app.py
@@ -4,7 +4,7 @@ import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 def log_result(result):
